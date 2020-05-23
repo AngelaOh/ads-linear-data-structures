f5070992a2a4e84ec20f005bd60d38fd2020f45a
Angela <angela.ohh@gmail.com>
2020-05-23 09:32:04 -0700

    callback and edit existing

--- src/data_structures/doubly_linked_list.js
+++ src/data_structures/doubly_linked_list.js
@@ -35,19 +35,18 @@ class DoublyLinkedList {
     let newNode = new this.Node(element);
 
     // new node's next is the old head
-    newNode.next = this.head;
+    newNode.next = this._head();
 
     // new node's prev is sentinel
     newNode.prev = this._sentinel;
 
     // old head's prev is new node
-    this._head.prev = newNode;
+    this._head().prev = newNode;
     
     // sentinel's next is new node
     this._sentinel.next = newNode;
 
-    // switch new nead
-    this._head = newNode;
+    return newNode;
   }
 
   insertTail(element) {
@@ -58,43 +57,60 @@ class DoublyLinkedList {
     newNode.next = this._sentinel;
 
     // new node's previous is old tail
-    newNode.prev = this.tail;
+    newNode.prev = this._tail();
 
     // old tail's next is new node 
-    this._tail.next = newNode;
+    this._tail().next = newNode;
 
     // sentinel's previous is new tail
     this._sentinel.prev = newNode;
-    
-    // switch old to new tail 
-    this._tail = newNode;
+
+    return newNode;
+
   }
 
   removeHead() {
-    // make sentinal's next be head.next
-    this._sentinel.next = this._head.next;
+    // // make sentinal's next be head.next
+    // this._sentinel.next = this._head.next;
 
-    // head next .previous is now sentinal
-    this._head.next.prev = this._sentinel;
+    // // head next .previous is now sentinal
+    // this._head.next.prev = this._sentinel;
 
     // make old head inactive 
-    this._head.remove();
+    return this._head().remove();
   }
 
   removeTail() {
-    this._sentinel.prev = this._tail.prev;
+    // this._sentinel.prev = this._tail.prev;
 
-    this.tail.prev.next = this._sentinel;
+    // this.tail.prev.next = this._sentinel;
 
-    this.tail.remove();
+    return this._tail().remove();
   }
 
   remove(node) {
-    
+    let currNode = this._head;
+    while (currNode != node) {
+      currNode = node.next;
+    }
+    let beforeCurr = currNode.prev;
+    let afterCurr = currNode.next;
 
+    beforeCurr.next = afterCurr;
+    afterCurr.prev = beforeCurr;
+
+    currNode.remove();
   }
 
   forEach(callback) {
+
+    let currNode = this._head();
+    let i = 0;
+    while (currNode !== this._sentinel) {
+      callback(currNode.element, i, this);
+      i += 1;
+      currNode = currNode.next;
+    }
   }
 
   count() {
